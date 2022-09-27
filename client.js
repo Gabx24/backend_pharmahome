864ccd72f6bd2fd832b68bbbbbfb695fb5faf90d
Gabriela <g.negran@yahoo.com>
2022-09-27 16:21:49 +0200

    add .env to render

---
+++ client.js
@@ -0,0 +1,8 @@
+const { Pool } = require("pg");
+const { ELEPHANT_SQL_CONNECTION_STRING } = process.env;
+
+const pool = new Pool();
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 8000;
+const { query } = require("./client.js");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  const { rows } = await query('SELECT * FROM "Products"');
+  console.log(rows);
+  res.json(rows);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await query(`SELECT * FROM "Products" WHERE id=$1`, [id]);
+  res.json(rows);
 });
 
 app.listen(port, () => {
